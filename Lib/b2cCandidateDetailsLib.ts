61996df664bc83e0b2c0b86bdf8d587a40de3438
Soni <SaraswatiJee.Soni@BritishCouncil.Org>
2020-03-04 08:40:02 +0530

    third commit

--- Lib/b2cCandidateDetailsLib.ts
+++ Lib/b2cCandidateDetailsLib.ts
@@ -16,9 +16,9 @@ export class b2cCandidateDetailsLib
         b2cCandidateDetailsPO.gender.click()
         waitStatementUtil.customWait1()
         let Selb2cEngNativeLang:selectUtil = new selectUtil(b2cCandidateDetailsPO.englishNativeLanguage);
-        waitStatementUtil.customWait1()
+        waitStatementUtil.customWait2()
         Selb2cEngNativeLang.selectByIndex(2)
-        waitStatementUtil.customWait1()
+        waitStatementUtil.customWait2()
         b2cCandidateDetailsPO.address.clear()
         waitStatementUtil.customWait1()
         b2cCandidateDetailsPO.address.sendKeys(RandomStringGeneratorUtil.getRandomString(5))
